c162beca83dfd1bbe734d1a22ca65c4e850a2abf
abidroid <abidroid1@gmail.com>
2022-05-11 21:18:44 +0500

    Initial Commit

---
+++ DmcApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  DmcApp
+//
+//  Created by MUHAMMAD ABID on 11/05/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ DmcApp/DmcAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  DmcAppApp.swift
+//  DmcApp
+//
+//  Created by MUHAMMAD ABID on 11/05/2022.
+//
+
+import SwiftUI
+
+@main
+struct DmcAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
